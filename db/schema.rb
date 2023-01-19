f3a3a50ef5c90c128e68a07f5de72ebc993b3100
Vincent Brunel <vincent.brunel20@gmail.com>
2023-01-19 16:37:00 +0100

    db

--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_22_165335) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "nickname", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -43,6 +42,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_22_165335) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
